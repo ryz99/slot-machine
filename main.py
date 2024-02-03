31e1e751c22a37cbb6aed58b6cdbadad4e92857b
ryz99 <rayaan.raza@tutanota.com>
2024-02-03 09:35:23 +0000

    added function to get bet with error check for bet that exceeds balance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 10
 
 
 #collect user deposit
@@ -30,10 +32,37 @@ def get_number_of_lines():
         
     return lines  
 
+def get_bet():
+    while True:
+        amount = input("Lay down a bet for each line ($" + str(MIN_BET) + " - $" + str(MAX_BET) + "): ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount<= MAX_BET:
+                break
+            else:
+                print(f"Bet must be between ${MIN_BET} - ${MAX_BET}!\n")
+        else:
+            print("Bet must be a valid number!\n")
+        
+    return amount
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance,lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet*lines
+        
+        if total_bet > balance:
+            print(f"You do not have sufficient balance. Current balance: ${balance}")
+        else:
+            break
+
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet: ${total_bet}\n")
+
+    print(balance,lines,bet)
 
 
 main()
