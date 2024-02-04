6278a7139e8e1bec966a8c7c0c641619337431ef
ryz99 <rayaan.raza@tutanota.com>
2024-02-04 10:03:16 +0000

    added symbols, added get_spin function, added print_spin function

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 10
 
+ROWS = 3
+COLS = 3
 
-#collect user deposit
+#number of symbols in each column
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+#get slot machine spin
+def get_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+    
+    return columns
+
+
+#transposes columns before printing
+def print_spin(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row])
+
+
+#get user deposit
 def deposit():
     while True:
         amount = input("Lay down a deposit ($): ")
@@ -18,6 +62,8 @@ def deposit():
         
     return amount
 
+
+#get number of lines
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + "): ")
@@ -32,6 +78,8 @@ def get_number_of_lines():
         
     return lines  
 
+
+#get user bet
 def get_bet():
     while True:
         amount = input("Lay down a bet for each line ($" + str(MIN_BET) + " - $" + str(MAX_BET) + "): ")
@@ -62,7 +110,9 @@ def main():
 
     print(f"You are betting ${bet} on {lines} lines. Total bet: ${total_bet}\n")
 
-    print(balance,lines,bet)
+    slots = get_spin(ROWS, COLS, symbol_count)
+    print_spin(slots)
+
 
 
 main()
